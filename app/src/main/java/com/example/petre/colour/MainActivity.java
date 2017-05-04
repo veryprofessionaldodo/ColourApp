82df579785bad1dce018870c7ac0a454b145f173
Pedro Daniel dos Santos Reis <petrexkgb@gmail.com>
2017-05-05 00:55:37 +0100

    Erro na performance, provavelmente por causa do onTouchListener
    Adicionado Quadrado onde vai ficar colour square

--- app/src/main/java/com/example/petre/colour/MainActivity.java
+++ app/src/main/java/com/example/petre/colour/MainActivity.java
@@ -5,26 +5,34 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView img;
+    ImageView img, sqr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         final View touchView = findViewById(R.id.full);
-
-        img = (ImageView) findViewById(R.id.ball);
+       // sqr = (ImageView) findViewById(R.id.square);
+        img = (ImageView) findViewById(R.id.circle);
 
         touchView.setOnTouchListener(new View.OnTouchListener() {
 
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 System.out.println(getSupportActionBar().getHeight() );
-                img.setY(event.getRawY() - getSupportActionBar().getHeight() - img.getHeight()/2 - getStatusBarHeight());
-                img.setX(event.getRawX() - img.getWidth()/2);
+
+
+               // Toast.makeText(MainActivity.this, "X" + event.getRawX() + " Width " + img.getWidth(), Toast.LENGTH_SHORT).show();
+                //if (((event.getRawX() - img.getWidth()/2) > sqr.getX()) && ((event.getRawX() - img.getWidth()/2 )> (sqr.getX()+ sqr.getWidth() ))) {
+               // if (event.getRawY() > sqr.getY() && event.getRawX() > sqr.getX()) {
+                   //if (event.getRawY() - img.getHeight()/2 > sqr.getY() && event.getRawY() - img.getHeight()/2 > sqr.getY()+ sqr.getHeight() ) {
+                        img.setY(event.getRawY() - getSupportActionBar().getHeight() - img.getHeight() / 2 - getStatusBarHeight());
+                        img.setX(event.getRawX() - img.getWidth() / 2);
+                 //   }
                 return true;
             }
         });
