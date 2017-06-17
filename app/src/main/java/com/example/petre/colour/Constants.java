a3628d085da2d7c8e9d933f91444a8c899267e98
Pedro Daniel dos Santos Reis <petrexkgb@gmail.com>
2017-06-17 12:41:49 +0100

    Problemas resolvidos na nas constraints

---
+++ app/src/main/java/com/example/petre/colour/Constants.java
@@ -0,0 +1,13 @@
+package com.example.petre.colour;
+
+/**
+ * Created by petre on 17/06/2017.
+ */
+
+public class Constants {
+
+    public static final float CIRCLE_DPS = 20;
+    public static final float WHEEL_DPS = 240;
+    public static final float SQUARE_DPS = 116;
+
+}
--- app/src/main/java/com/example/petre/colour/MainActivity.java
+++ app/src/main/java/com/example/petre/colour/MainActivity.java
@@ -27,11 +27,33 @@ public class MainActivity extends AppCompatActivity {
     private ImageView circle;
     private Bitmap bitmap;
     private Square square;
+    private boolean hasClickedOnRing = false;
+    private boolean hasClickedOnSquare = false;
+    private int pixelWidth, pixelHeight;
+
+    private float SCALE;
+    int widthCircleMax;
+    int heightCircleMax;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setVariables();
+
+        circle.setY(.458f * pixelHeight / 2 - dpsToPixels(Constants.CIRCLE_DPS/2));
+        circle.setX(.5f * pixelWidth / 2 - dpsToPixels(Constants.CIRCLE_DPS/2));
+
+        //ImageView img = (ImageView) findViewById(R.id.square_outline);
+        //System.out.println("X " + img.getX() + (float)img.getWidth()/2.0f + " Y " + img.getY() + (float) img.getHeight()/2.0f);
+
+        setTouchListener();
+    }
+
+    public void setVariables() {
+        ImageView background = (ImageView) findViewById(R.id.background);
+        background.setScaleType(ImageView.ScaleType.FIT_XY);
 
         square = (Square) findViewById(R.id.square_color);
         square.setColor(Color.BLUE);
@@ -41,28 +63,20 @@ public class MainActivity extends AppCompatActivity {
         touchView.setDrawingCacheEnabled(true);
         touchView.buildDrawingCache();
 
-
         int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
         touchView.setSystemUiVisibility(uiOptions);
         circle = (ImageView) findViewById(R.id.circle);
 
+        SCALE = touchView.getContext().getResources().getDisplayMetrics().density;
+
         Display display = getWindowManager().getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
-        int width = size.x;
-        int height = size.y;
+        pixelWidth = size.x;
+        pixelHeight = size.y;
 
-        circle.setY(.458f * height/2 - circle.getHeight()/2);
-        circle.setX(.5f* width/2 - circle.getWidth()/2);
-
-        ImageView img = (ImageView) findViewById(R.id.square_outline);
-        System.out.println("X " + img.getX() + (float)img.getWidth()/2.0f + " Y " + img.getY() + (float) img.getHeight()/2.0f);
-
-        setTouchListener();
-
-        ImageView background = (ImageView) findViewById(R.id.background);
-
-        background.setScaleType(ImageView.ScaleType.FIT_XY);
+        widthCircleMax = dpsToPixels(Constants.WHEEL_DPS);
+        heightCircleMax = dpsToPixels(Constants.WHEEL_DPS);
 
     }
 
@@ -72,28 +86,49 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
 
-                if (event.getY() > 10) { // TODO Aqui é a imagem da roda
-                    bitmap = touchView.getDrawingCache();
+                float delta_x = event.getX() - pixelWidth / 2.0f;
+                float delta_y = event.getY() - .458f * pixelHeight;
+                System.out.println(delta_x + " " + delta_y);
+                System.out.println(pixelWidth / 2.0f + " " + .458f * pixelHeight);
+                float angle = (float) Math.atan2(delta_y, delta_x);
 
-                    circle.setY(event.getY() - circle.getHeight() / 2);
-                    circle.setX(event.getX() - circle.getWidth() / 2);
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    hasClickedOnRing = false;
+                    hasClickedOnSquare = false;
 
-                    int pixel = bitmap.getPixel((int) event.getX(), (int) event.getY());
-
-                    int r = Color.red(pixel);
-                    int g = Color.green(pixel);
-                    int b = Color.blue(pixel);
-
-                    square.setColor(Color.rgb(r, g, b));
-                }
-                else if (event.getX() > 10){ // TODO Aqui é a imagem do quadrado
-
+                } else {
+                    if (event.getY() > 10 || hasClickedOnRing) { // TODO Aqui é a imagem da roda
+                        handleChangeColor(event);
+                        hasClickedOnRing = true;
+                    } else if (event.getX() > 10) { // TODO Aqui é a imagem do quadrado
+                        hasClickedOnSquare = true;
+                    }
                 }
                 return true;
             }
 
+
         });
 
+    }
+
+    public void handleChangeColor(MotionEvent event) {
+        bitmap = touchView.getDrawingCache();
+
+        circle.setY(event.getY() - dpsToPixels(Constants.CIRCLE_DPS/2));
+        circle.setX(event.getX() - dpsToPixels(Constants.CIRCLE_DPS/2));
+
+        int pixel = bitmap.getPixel((int) event.getX(), (int) event.getY());
+
+        int r = Color.red(pixel);
+        int g = Color.green(pixel);
+        int b = Color.blue(pixel);
+
+        square.setColor(Color.rgb(r, g, b));
+    }
+
+    public int dpsToPixels(float dps) {
+        return (int) (dps * SCALE + 0.5f);
     }
 }
 /*
@@ -101,4 +136,11 @@ Matrix matrix = new Matrix();
 imageView.setScaleType(ImageView.ScaleType.MATRIX);   //required
 matrix.postRotate((float) angle, pivotX, pivotY);
 imageView.setImageMatrix(matrix);
+ */
+
+/*
+ TODO LIST :
+    TODO COORDENADAS NA RODA
+    TODO COORDENADAS NO QUADRADO
+    TODO PROBLEMA NO NEXUS 6, PROVAVELMENTE XXXHDPI DEMASIADO PEQUENO
  */
